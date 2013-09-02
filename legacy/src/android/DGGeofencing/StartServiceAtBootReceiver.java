2dd39e019f57241a2f1211ab2a5a80c2b1639821
Erik Jan de Wit <erikjan.dewit@gmail.com>
2013-09-02 16:43:06 +0200

    removed debugging

--- legacy/src/android/DGGeofencing/StartServiceAtBootReceiver.java
+++ legacy/src/android/DGGeofencing/StartServiceAtBootReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import android.widget.Toast;
 
 /**
  * Created by edewit on 9/2/13.
@@ -13,8 +12,5 @@ public class StartServiceAtBootReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
         Intent serviceLauncher = new Intent(context, DGGeofencingService.class);
         context.startService(serviceLauncher);
-        Log.v("TEST", "Service loaded at start");
-        Log.d("TEST", "------------------------------- starting up ------------------------------");
-        Toast.makeText(context, "service stated", 1000).show();
     }
 }
